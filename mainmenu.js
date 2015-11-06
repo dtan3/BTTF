5640cac8512ae8d5d44bd66e4dff7715e25f32c0
David Tan <missionbit@Missions-MacBook-Air-3.local>
2015-11-05 18:02:05 -0800

    updated main menu load

---
+++ mainmenu.js
@@ -0,0 +1,24 @@
+
+var positionClouds=function(top){
+    //positions the clouds randomnly
+    positionClouds(top);
+}
+var main = {
+    preload: function () {
+    game.load.image=('title','images/title.png');  
+        
+    },
+    create: function () {    
+    game.stage.backgroundColor ='#000';
+    
+    var title= game.add.sprite(50,10,'title');
+    
+    
+    },
+    update: function () {
+            
+    }
+};
+
+
+
